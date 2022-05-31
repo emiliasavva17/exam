86e51b32776a5e3a96c9d4fa706fa2bde14bb891
emiliasavva17 <61870162+emiliasavva17@users.noreply.github.com>
2022-05-31 07:03:13 +0200

    Update seed.jsx

--- app/routes/seed.jsx
+++ app/routes/seed.jsx
@@ -1,12 +1,20 @@
 import { redirect, json } from "@remix-run/node";
 import { Form, useLoaderData, Link } from "@remix-run/react";
 import connectDb from "~/db/connectDb.server";
+
 import {
   requireUserSession,
   getSession,
   commitSession,
 } from "~/sessions.server";
 import bcrypt from "bcryptjs";
+
+export async function loader({ request }) {
+  const db = await connectDb();
+  const Profile = await db.models.Profiles.find();
+  console.log(Profile.length);
+  return json({ len: Profile.length });
+}
 
 export async function action({ request }) {
   const db = await connectDb();
@@ -16,10 +24,10 @@ export async function action({ request }) {
   const education = await db.models.Education;
   const courses = await db.models.Courses;
 
-  await users.deleteMany();
-  await profiles.deleteMany();
-  await education.deleteMany();
-  await courses.deleteMany();
+  // await users.deleteMany();
+  // await profiles.deleteMany();
+  // await education.deleteMany();
+  // await courses.deleteMany();
 
   var pwd = bcrypt.hashSync("test1", 10);
 
@@ -51,12 +59,12 @@ export async function action({ request }) {
     user_type: "student",
   });
 
-  newProfile = await db.models.Students.create({
+  newProfile = await db.models.Profiles.create({
     userId: newUser._id,
     name: "Dima Preda",
     date: new Date(),
     bio: "Ex commodi corrupti et quia aliquid in quis rerum. Est quia optio nam quia galisum et veritatis omnis. Sit ullam laborum et unde voluptate ea deleniti et consequatur dolorem in praesentium perferendis est tempore nulla. Non debitis quibusdam sit impedit error non omnis rerum. ",
-    profile_img:
+    profilePic:
       "https://images.unsplash.com/photo-1500048993953-d23a436266cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
     tags: "Ruby, Kotlin, MySQL,Python,Algorithms,Dynamic Programming, Binary Trees, OOP",
     linkedIn: "https://www.linkedin.com",
@@ -70,12 +78,12 @@ export async function action({ request }) {
     user_type: "student",
   });
 
-  newProfile = await db.models.Students.create({
+  newProfile = await db.models.Profiles.create({
     userId: newUser._id,
     name: "Anastasia Ivanova",
     date: new Date(),
     bio: "Quo architecto illum est neque iste et dolorum quisquam. Cum soluta adipisci id omnis laudantium sit nemo deserunt et laborum aspernatur non accusamus dolorem aut accusamus placeat. Cum iure recusandae ut velit quia aut assumenda dolor. Et eveniet dolorum ex quas voluptas aut provident impedit? ",
-    profile_img:
+    profilePic:
       "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
     tags: "Ruby,some,random,words, Kotlin, MySQL,Python,Algorithms,Dynamic Programming, Binary Trees, OOP",
     linkedIn: "https://www.linkedin.com",
@@ -89,12 +97,12 @@ export async function action({ request }) {
     user_type: "student",
   });
 
-  newProfile = await db.models.Students.create({
+  newProfile = await db.models.Profiles.create({
     userId: newUser._id,
     name: "Violeta Deharo",
     date: new Date(),
     bio: "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ",
-    profile_img:
+    profilePic:
       "https://images.unsplash.com/photo-1554727242-741c14fa561c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     tags: "Html,some,random,words, Kotlin, MySQL,Python,Java,Dynamic Programming, CSS, OOP",
     linkedIn: "https://www.linkedin.com",
@@ -108,27 +116,48 @@ export async function action({ request }) {
     user_type: "student",
   });
 
-  newProfile = await db.models.Students.create({
+  newProfile = await db.models.Profiles.create({
     userId: newUser._id,
-    name: "Aldred Ursu",
+    name: "Alfred Ursu",
     date: new Date(),
     bio: "Qui sint accusamus est quibusdam optio hic dolor voluptas. Eos omnis omnis non neque pariatur et voluptas corporis aut consequatur mollitia? Ut eius temporibus qui illo temporibus et eius dolore aut repellendus mollitia in eligendi nostrum aut assumenda culpa est aspernatur molestias. ",
-    profile_img:
+    profilePic:
       "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
     tags: "C#,C++,Basics,Remix,Ionic,Python,Angular",
     linkedIn: "https://www.linkedin.com",
     portfolio: "https://github.com/",
   });
 
-  return redirect("/logout");
+  return redirect("/");
 }
 
 export default function Seed() {
+  const { len } = useLoaderData();
+  console.log(len, "len");
   return (
     <div className="seed-page">
       <Form method="POST" className="">
-        <button type="submit">Seed the database</button>
+        <h1 className="text-center">
+          Are you sure that you want to see your database
+        </h1>{" "}
+        <br />
+        <br />
+        <div className="flex justify-evenly content-center">
+          <Link
+            to="/"
+            className=" text-white bg-red-800 rounded py-2 px-4 ml-10"
+          >
+            No
+          </Link>
+          <button
+            type="submit"
+            className="mr-10 text-white bg-green-800 rounded px-4 py-2"
+          >
+            Seed the database
+          </button>
+        </div>
       </Form>
+      <br />
       it is better to creat yourself at least oe profile and in the update you
       can add courses and education
     </div>
